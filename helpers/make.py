ce93e59f6f410e8ba6bafb42c77828ad6192ede7
David J. Malan <malan@harvard.edu>
2016-08-25 19:51:10 -0400

    added 'No rule to make target'

--- helpers/make.py
+++ helpers/make.py
@@ -6,6 +6,15 @@ def help(lines):
     if matches:
         after = [
             "Looks like you already compiled `{}` and haven't made any changes to `{}.c` since.".format(matches.group(1), matches.group(1)),
-            "Or did you forget to save `{}.c` before running `make`?".format(matches.group(1)),
+            "Or did you forget to save `{}.c` before running `make`?".format(matches.group(1))
+        ]
+        return (lines[0:1], after)
+
+    # No rule to make target
+    matches = re.search(r"^make: \*\*\* No rule to make target `(.+)'.  Stop.", lines[0])
+    if matches:
+        after = [
+            "Do you actually have a file called `{}`.c?".format(matches.group(1)),
+            "If using a Makefile, are you sure you have a target called `{}`?".format(matches.group(1))
         ]
         return (lines[0:1], after)
