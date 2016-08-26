64df3fa00b1d3fe7e1ce0a33a1e5fa857fc4d1dc
Brian Yu <brianyu28@gmail.com>
2016-08-26 15:21:44 -0400

    add new matchers, add filenames with line numbers

--- helpers/bash.py
+++ helpers/bash.py
@@ -37,3 +37,36 @@ def help(lines):
         else:
             after.append("Did you remember to make `{}` \"executable\" with `chmod +x {}`?".format(matches.group(1), matches.group(1)))
         return (lines[0:1], after)
+    
+    # Remove file
+    matches = re.search(r"^rm: remove regular (?:empty )?file ‘(.+)’\?", lines[0])
+    if matches:
+        empty = re.search(r"^rm: remove regular empty file", lines[0])
+        after = [
+            "The command you typed will remove the file `{}`.".format(matches.group(1))
+        ]
+        if empty:
+            after.append("`{}` is an empty file.".format(matches.group(1)))
+        after.append("If you wish to remove `{}`, type `y` and press return.".format(matches.group(1)))
+        after.append("Typing `n` and pressing return will cancel the operation.")
+        return (lines[0:1], after)
+    
+    # mv overwrite
+    matches = re.search(r"^mv: overwrite ‘(.+)’\?", lines[0])
+    if matches:
+        after = [
+            "You are moving a file to `{}`, but there is already a file with the same name there.".format(matches.group(1)),
+            "To continue with the move operation and replace the old file, type `y` and press return.",
+            "Typing `n` and pressing return will cancel the operation."
+        ]
+        return (lines[0:1], after)
+    
+    # cp overwrite
+    matches = re.search(r"^cp: overwrite ‘(.+)’\?", lines[0])
+    if matches:
+        after = [
+            "You are copying a file to `{}`, but there is already a file with the same name there.".format(matches.group(1)),
+            "To continue with the copy operation and replace the old file, type `y` and press return.",
+            "Typing `n` and pressing return will cancel the operation."
+        ]
+        return (lines[0:1], after)
--- helpers/make.py
+++ helpers/make.py
@@ -18,3 +18,11 @@ def help(lines):
             "If using a Makefile, are you sure you have a target called `{}`?".format(matches.group(1))
         ]
         return (lines[0:1], after)
+    
+    # nothing to be done for
+    matches = re.search(r"^make: Nothing to be done for `([^']+).c'.", lines[0])
+    if matches:
+        after = [
+            "Try compiling your program with `make {}` rather than `make {}.c`.".format(matches.group(1), matches.group(1))
+        ]
+        return (lines[0:1], after)
