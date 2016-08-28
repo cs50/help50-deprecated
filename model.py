c85cc3007dc8bbc5317e8417a8528c406e81e5e5
Brian Yu <brianyu28@gmail.com>
2016-08-28 01:07:45 -0400

    configure inputs/outputs tables

---
+++ model.py
@@ -0,0 +1,42 @@
+import MySQLdb as mdb
+import os
+from warnings import filterwarnings
+
+# establish connection to database
+con = mdb.connect(
+    os.environ['MYSQL_HOST'],
+    os.environ['MYSQL_USER'],
+    os.environ['MYSQL_PASS'],
+    os.environ['MYSQL_DATABASE'],
+    charset='utf8'
+)
+cur = con.cursor()
+
+# disable 'table already exists' warnings
+filterwarnings('ignore', category=mdb.Warning)
+
+def configure():
+    inputs_schema = '''
+    CREATE TABLE IF NOT EXISTS inputs (
+        id BIGINT NOT NULL AUTO_INCREMENT,
+        cmd VARCHAR(1024) NULL,
+        script TEXT NOT NULL,
+        username VARCHAR(32) NULL,
+        created DATETIME DEFAULT CURRENT_TIMESTAMP,
+        PRIMARY KEY (id)
+    ) ENGINE=InnoDB;
+    '''
+    
+    outputs_schema = '''
+    CREATE TABLE IF NOT EXISTS outputs (
+        id BIGINT NOT NULL AUTO_INCREMENT,
+        input_id BIGINT,
+        output TEXT,
+        PRIMARY KEY (id),
+        FOREIGN KEY (input_id) REFERENCES inputs(id) ON DELETE CASCADE
+    ) ENGINE=InnoDB;
+    '''
+    
+    cur.execute(inputs_schema)
+    cur.execute(outputs_schema)
+
