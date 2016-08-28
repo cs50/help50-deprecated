5409d36bdd232fbedfd4c2f00da41ae388918e43
Brian Yu <brianyu28@gmail.com>
2016-08-28 00:37:08 -0400

    add 'nothing to be done for' matcher to 'make'

--- helpers/make.py
+++ helpers/make.py
@@ -1,7 +1,8 @@
 import re
 def help(lines):
 
-    # is up to date.
+    # $ make foo
+    # make: `foo' is up to date.
     matches = re.search(r"^make: `(.+)' is up to date.", lines[0])
     if matches:
         after = [
@@ -10,11 +11,21 @@ def help(lines):
         ]
         return (lines[0:1], after)
 
-    # No rule to make target
+    # $ make foo
+    # make: *** No rule to make target `foo'.  Stop.
     matches = re.search(r"^make: \*\*\* No rule to make target `(.+)'.  Stop.", lines[0])
     if matches:
         after = [
             "Do you actually have a file called `{}`.c?".format(matches.group(1)),
             "If using a Makefile, are you sure you have a target called `{}`?".format(matches.group(1))
+        ]
+        return (lines[0:1], after)
+    
+    # $ make foo.c
+    # make: Nothing to be done for `foo.c'.
+    matches = re.search(r"^make: Nothing to be done for `([^']+).c'.", lines[0])
+    if matches:
+        after = [
+            "Try compiling your program with `make {}` rather than `make {}.c`.".format(matches.group(1), matches.group(1))
         ]
         return (lines[0:1], after)
