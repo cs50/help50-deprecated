f4dc3faada4841d147eac8ddfe29977f6b62274f
Brian Yu <brianyu28@gmail.com>
2016-08-28 00:29:23 -0400

    more whitespace fixes in cp.py

--- helpers/cp.py
+++ helpers/cp.py
@@ -12,7 +12,7 @@ def help(lines):
         # if "/" is present in destination path, then assume copying to a new directory
         new_dir = "/" in matches.group(1)
         interpretation = "You are copying a file to `{}`, but there is already a file ".format(matches.group(1))
-        interpretation += "there " if new_dir else " in the current directory "
+        interpretation += "there " if new_dir else "in the current directory "
         interpretation += "with the same name."
         
         after = [interpretation]
