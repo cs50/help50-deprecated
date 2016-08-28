9ca33ad8c88141d9be2ccf37435e8c17d4c9b819
Brian Yu <brianyu28@gmail.com>
2016-08-28 00:17:27 -0400

    add moving/renaming distinction in mv.py

--- helpers/mv.py
+++ helpers/mv.py
@@ -3,11 +3,22 @@ def help(lines):
    
     # $ mv foo bar
     # mv: overwrite ‘bar’?
+    #
+    # $ mv foo baz/foo
+    # mv: overwrite ‘baz/foo’?
     matches = re.search(r"^mv: overwrite ‘(.+)’\?", lines[0])
     if matches:
-        after = [
-            "You are moving a file to `{}`, but there is already a file with the same name there.".format(matches.group(1)),
-            "To continue with the move operation and replace the old file, type `y` and press return.",
-            "Typing `n` and pressing return will cancel the operation."
-        ]
+        
+        # if a "/" is present in the destination path, assume moving a file; otherwise, renaming a file
+        moving = "/" in matches.group(1)
+        
+        interpretation = "You are "
+        interpretation += "moving" if moving else "renaming"
+        interpretation += " a file to `{}`, but there is already a file with the same name".format(matches.group(1))
+        interpretation += " there." if moving else " in the current directory."
+        
+        after = [interpretation]
+        after.append("To replace the old file, type `y` and press return.")
+        after.append("Typing `n` and pressing return will cancel the operation.")
+        
         return (lines[0:1], after)
