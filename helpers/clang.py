b038f8695a5306d2a47b751c5bfe40ea72c746cf
David J. Malan <malan@harvard.edu>
2016-09-30 01:32:55 -0400

    simplified some language

--- helpers/clang.py
+++ helpers/clang.py
@@ -302,8 +302,8 @@ def help(lines):
         response = [
             "Looks like `clang` is having some trouble understanding where your functions start and end in your code.",
             "Are you defining a function (like `main` or some other function) somewhere just before line {} of `{}`?".format(matches.line, matches.file),
-            "If so, make sure the function header (the line that introduces the name of the function) doesn't end with a semicolon.",
-            "Also check to make sure that all of the code for your function is inside of curly braces."
+            "If so, make sure the function's first line doesn't end with a semicolon.",
+            "Also make sure that all of the code for your function is inside of curly braces."
         ]
         return (lines[0:1], response)
 
