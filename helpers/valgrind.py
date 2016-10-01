505382f443c835d7775bdb9117e6bf8b18650929
David J. Malan <malan@harvard.edu>
2016-10-01 14:45:21 -0400

    Revamped issue_locate as frame_extract (for consistency with clang's semantics) so as to display frames on which help is based.

--- helpers/valgrind.py
+++ helpers/valgrind.py
@@ -3,44 +3,44 @@ from collections import namedtuple
 
 def help(lines):
 
-    # check for valgrind's output
+    # check for recognized output
     if not re.search(r"^==\d+== Memcheck, a memory error detector$", lines[0]):
         return
 
-    # iterate over lines
+    # iterate over lines ourselves
     for i, line in enumerate(lines):
 
-        # use of uninitialized value of size 8
+        # Use of uninitialized value of size 8
         matches = re.search(r"^==\d+== Use of uninitialised value of size (\d+)$", line)
         if matches:
             response = [
                 "Looks like you're trying to use a {}-byte variable that might not have a value?".format(matches.group(1)),
             ]
-            issue = issue_locate(lines[i+1:])
-            if issue:
-                if issue.line:
-                    response.append("Take a closer look at line {} of `{}`.".format(issue.line, issue.file))
+            frames, frame = frame_extract(lines[i+1:])
+            if frame:
+                if frame.line:
+                    response.append("Take a closer look at line {} of `{}`.".format(frame.line, frame.file))
                 else:
-                    response.append("Take a closer look at `{}`.".format(issue.function))
+                    response.append("Take a closer look at `{}`.".format(frame.function))
                     response.append("And be sure to compile your program with `-ggdb3` to see line numbers in `valgrind`'s output.")
-            return (lines[i:i+1], response)
+            return (lines[i:i+1+frames], response)
 
-        # invalid write of size 4
+        # Invalid write of size 4
         matches = re.search(r"^==\d+== Invalid write of size (\d+)$", line)
         if matches:
             bytes = "bytes" if int(matches.group(1)) > 1 else "byte"
             response = [
-                "Looks like you're trying to store {} {} at a location in memory that you're not allowed to access?".format(matches.group(1), bytes),
+                "Looks like you're trying modify {} {} of memory that isn't yours?".format(matches.group(1), bytes),
                 "Did you try to store something beyond the bounds of an array?"
             ]
-            issue = issue_locate(lines[i+1:])
-            if issue:
-                if issue.line:
-                    response.append("Take a closer look at line {} of `{}`.".format(issue.line, issue.file))
+            frames, frame = frame_extract(lines[i+1:])
+            if frame:
+                if frame.line:
+                    response.append("Take a closer look at line {} of `{}`.".format(frame.line, frame.file))
                 else:
-                    response.append("Take a closer look at `{}`.".format(issue.function))
+                    response.append("Take a closer look at `{}`.".format(frame.function))
                     response.append("And be sure to compile your program with `-ggdb3` to see line numbers in `valgrind`'s output.")
-            return (lines[i:i+1], response)
+            return (lines[i:i+1+frames], response)
 
         # 40 bytes in 1 blocks are definitely lost in loss record 1 of 1
         matches = re.search(r"^==\d+== (\d+) bytes in (\d+) blocks are definitely lost in loss record (\d+) of (\d+)$", line)
@@ -50,17 +50,16 @@ def help(lines):
                 "Looks like your program leaked {} {} of memory.".format(matches.group(1), bytes),
                 "Did you forget to `free` memory that you allocated via `malloc`?"
             ]
-            issue = issue_locate(lines[i+1:])
-            if issue:
-                if issue.line:
-                    response.append("Take a closer look at line {} of `{}`.".format(issue.line, issue.file))
+            frames, frame = frame_extract(lines[i+1:])
+            if frame:
+                if frame.line:
+                    response.append("Take a closer look at line {} of `{}`.".format(frame.line, frame.file))
                 else:
-                    response.append("Take a closer look at `{}`.".format(issue.function))
+                    response.append("Take a closer look at `{}`.".format(frame.function))
                     response.append("And be sure to compile your program with `-ggdb3` to see line numbers in `valgrind`'s output.")
-            return (lines[i:i+1], response)
+            return (lines[i:i+1+frames], response)
 
-
-    # iterate over lines again (for less precise errors)
+    # iterate over lines ourselves again (for less precise errors)
     for i, line in enumerate(lines):
 
         # Conditional jump or move depends on uninitialised value(s)
@@ -69,12 +68,12 @@ def help(lines):
             response = [
                 "Looks like you're trying to use a variable that might not have a value?",
             ]
-            issue = issue_locate(lines[i+1:])
-            if issue:
-                if issue.line:
-                    response.append("Take a closer look at line {} of `{}`.".format(issue.line, issue.file))
+            frames, frame = frame_extract(lines[i+1:])
+            if frame:
+                if frame.line:
+                    response.append("Take a closer look at line {} of `{}`.".format(frame.line, frame.file))
                 else:
-                    response.append("Take a closer look at `{}`.".format(issue.function))
+                    response.append("Take a closer look at `{}`.".format(frame.function))
                     response.append("And be sure to compile your program with `-ggdb3` to see line numbers in `valgrind`'s output.")
             return (lines[i:i+1], response)
 
@@ -91,34 +90,35 @@ def help(lines):
                 response.append("Run `valgrind --leak-check=full {}` for more details.".format(matches.group(1)))
             return (lines[i:i+1], response)
 
-# finds the function, file, and line of an issue
-def issue_locate(lines):
+# Parses lines for stack frames, returning (frames, frame), where frames is the number of frames parsed,
+# and frame is a tuple with address, function, file, and line fields representing the likely source of an error.
+def frame_extract(lines):
 
-    # identify possible locations of issue
-    locations = []
-    Location = namedtuple('Location', 'address function file line')
+    # identify possible frames
+    frames = []
+    Frame = namedtuple("Frame", ["address", "function", "file", "line"])
     for line in lines:
         matches = re.search(r"^==\d+==    (?:at|by) (0x[0-9A-Fa-f]+): (.+) \((.+?)(?::(\d+))?\)", line)
         if matches:
-            locations.append(Location(matches.group(1), matches.group(2), matches.group(3), matches.group(4)))
+            frames.append(Frame(address=matches.group(1), function=matches.group(2), file=matches.group(3), line=matches.group(4)))
         else:
             break
 
-    # infer actual location of issue
-    locations.reverse()
-    for i in range(len(locations)-1):
+    # infer actual frame
+    frames.reverse()
+    for i in range(len(frames)-1):
 
         # at 0x4C2AB80: malloc (in /usr/lib/valgrind/vgpreload_memcheck-amd64-linux.so)
         # by 0x400546: foo (foo.c:6)
         # by 0x400568: main (foo.c:12)
-        if (locations[i].line and not locations[i+1].line):
-            return locations[i]
+        if (frames[i].line and not frames[i+1].line):
+            return len(frames), frames[i]
 
         # at 0x4C2AB80: malloc (in /usr/lib/valgrind/vgpreload_memcheck-amd64-linux.so)
         # by 0x400546: foo (in /srv/www/foo)
         # by 0x400568: main (in /srv/www/foo)
-        if (not locations[i].line and locations[i].file != locations[i+1].file):
-            return locations[i]
+        if (not frames[i].line and frames[i].file != frames[i+1].file):
+            return len(frames), frames[i]
 
         # at 0x508299B: _itoa_word (_itoa.c:179)
         # by 0x5086636: vfprintf (vfprintf.c:1660)
@@ -126,9 +126,9 @@ def issue_locate(lines):
         # by 0x5082DFD: vfprintf (vfprintf.c:1313)
         # by 0x508D3D8: printf (printf.c:33)
         # by 0x40054C: main (foo.c:6)
-        if (locations[i].line and locations[i+1].line and len(locations[i].address) < len(locations[i+1].address)):
-            return locations[i]
+        if (frames[i].line and frames[i+1].line and len(frames[i].address) < len(frames[i+1].address)):
+            return len(frames), frames[i]
 
     # at 0x40054F: foo (foo.c:7)
     # by 0x400568: main (foo.c:12)
-    return locations[len(locations)-1] if locations else None
+    return (len(frames), frames[len(frames)-1]) if frames else (0, None)
