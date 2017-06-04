f546bb8c2b20b865b504829077bc8827be61f511
Chad Sharp <crossroads1112@gmail.com>
2017-06-04 09:49:01 -0400

    Port help50 to Python

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name="help50-client",
+      version="2.0",
+      description="This is help50, a command-line tool that helps students understand error messages.",
+      url="https://github.com/cs50/help50",
+      install_requires = ["requests", "termcolor"])
+
+
