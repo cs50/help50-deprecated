32d66180d151626073146374bcd30b354d4597c7
Kareem Zidane <kzidane@cs50.harvard.edu>
2017-08-08 19:06:26 -0400

    included message catalogs

--- setup.py
+++ setup.py
@@ -1,4 +1,23 @@
+from glob import glob
+from os import walk
+from os.path import join, splitext
 from setuptools import setup
+from subprocess import call
+
+def create_mo_files():
+    """Compiles .po files in local/LANG to .mo files and returns them as array of data_files"""
+
+    mo_files=[]
+    for prefix in glob("locale/*"):
+        for _,_,files in walk(prefix):
+            for file in files:
+                if file.endswith(".po"):
+                    po_file = join(prefix, file)
+                    mo_file = splitext(po_file)[0] + ".mo"
+                    call(["msgfmt", "-o", mo_file, po_file])
+                    mo_files.append((join("help50", prefix, "LC_MESSAGES"), [mo_file]))
+
+    return mo_files
 
 setup(
     author="CS50",
@@ -15,6 +34,7 @@ setup(
     keywords="help50",
     name="help50",
     scripts=["help50"],
+    data_files=create_mo_files(),
     url="https://github.com/cs50/help50",
     version="2.0.0"
 )
