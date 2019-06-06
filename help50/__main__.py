73317d5131ad9dcefc5b75cdddcd98c4538f6c5f
Chad Sharp <chad@cs50.harvard.edu>
2019-06-06 11:41:03 -0400

    add standard polish

--- help50/__main__.py
+++ help50/__main__.py
@@ -11,6 +11,7 @@ import signal
 import sys
 import textwrap
 import tempfile
+import traceback
 
 from argparse import ArgumentParser, REMAINDER
 import lib50
@@ -18,10 +19,35 @@ import requests
 import termcolor
 import pexpect
 
-from . import HELPERS, PRE_HELPERS
+from . import HELPERS, PRE_HELPERS, __version__
 
 lib50.api.LOCAL_PATH = "~/.local/share/help50"
 
+
+class Error(Exception):
+    pass
+
+
+def excepthook(cls, exc, tb):
+    if (issubclass(cls, lib50.Error) or issubclass(cls, Error)) and exc.args:
+        termcolor.cprint(str(exc), "red", file=sys.stderr)
+    elif issubclass(cls, FileNotFoundError):
+        termcolor.cprint(f"{exc.filename} not found", "red", file=sys.stderr)
+    elif issubclass(cls, KeyboardInterrupt):
+        print()
+    elif not issubclass(cls, Exception):
+        return
+    else:
+        termcolor.cprint("Sorry, something's wrong! Let sysadmins@cs50.harvard.edu know!", "red", file=sys.stderr)
+
+    if excepthook.verbose:
+        traceback.print_exception(cls, exc, tb)
+
+    sys.exit(1)
+
+
+excepthook.verbose = True
+sys.excepthook = excepthook
 
 @contextlib.contextmanager
 def syspath(newpath):
@@ -49,23 +75,28 @@ def load_config(dir):
     return options
 
 
-def load_helpers(slug, offline=False):
-    helpers_dir = lib50.local(slug, "help50")
+def load_helpers(slug):
+    try:
+        helpers_dir = lib50.local(slug, "help50")
+    except lib50.Error:
+        raise Error("Failed to fetch helpers, please ensure that you are connected to the internet!")
+
     config = load_config(helpers_dir)
     for helper in config["helpers"]:
         with syspath([str(helpers_dir)]):
-            __import__(helper)
+            try:
+                __import__(helper)
+            except ImportError:
+                raise Error("Failed to load helpers, please let sysadmins@cs50.harvard.edu know!")
 
 
 
-def get_help(output, *domains):
-    if not domains:
-        domains = HELPERS.keys()
 
+def get_help(output):
     # Strip ANSI codes
     output = re.sub(r"\x1B\[[0-?]*[ -/]*[@-~]", "", output)
 
-    for domain in domains:
+    for domain in HELPERS.keys():
         lines = output
         for pre_helper in PRE_HELPERS.get(domain, []):
             lines = pre_helper(lines)
@@ -83,7 +114,7 @@ def get_help(output, *domains):
 
 def render_help(help):
     if help is None:
-        termcolor.cprint("help50 does not yet know how to help with this!", "yellow")
+        termcolor.cprint("Sorry, help50 does not yet know how to help with this!", "yellow")
     else:
         for line in help[0]:
             termcolor.cprint(line, "grey", "on_yellow")
@@ -92,116 +123,35 @@ def render_help(help):
 
 
 def main():
-    def handler(signum, frame):
-        """Return 1 on Ctrl-C"""
-        print("")
-        sys.exit(1)
-
-    signal.signal(signal.SIGINT, handler)
-
     parser = ArgumentParser(description="A command-line tool that helps "
                                         "students understand error messages.")
-    parser.add_argument("-i", "--interactive",
-                        help="allow error messages to be written to "
-                              "stdin interactively",
-                        action="store_true")
     parser.add_argument("-s", "--slug", help="identifier indicating from where to download helpers", default="cs50/helpers/master")
-    parser.add_argument("--offline", help="run help50 offline", action="store_true")
+    parser.add_argument("-v", "--verbose", help="display the full tracebacks of any errors")
+    parser.add_argument("-V", "--version", action="version", version=f"%(prog)s {__version__}")
     parser.add_argument("command", nargs=REMAINDER,
                         default=[], help="command to be run")
     args = parser.parse_args()
 
-    load_helpers(args.slug, args.offline)
+    excepthook.verbose = args.verbose
 
-    if args.command:
-        # Capture stdout and stderr from process, and print it out
-        with tempfile.TemporaryFile(mode="r+b") as temp:
-            proc = pexpect.spawn(f"bash -lc \"{' '.join(shlex.quote(word) for word in args.command)}\"", env=os.environ)
-            proc.logfile_read = temp
-            proc.interact()
-            proc.close()
+    load_helpers(args.slug)
 
-            temp.seek(0)
-            script = temp.read().decode().replace("\r\n", "\n")
+    if not args.command:
+        raise Error("Careful, you forgot to tell me with which command you need help!")
 
-    # Interactive stdin
-    elif args.interactive:
-        script = sys.stdin.read()
+    # Capture stdout and stderr from process, and print it out
+    with tempfile.TemporaryFile(mode="r+b") as temp:
+        proc = pexpect.spawn(f"bash -lc \"{' '.join(shlex.quote(word) for word in args.command)}\"", env=os.environ)
+        proc.logfile_read = temp
+        proc.interact()
+        proc.close()
 
-    # Non-interactive stdin
-    elif not sys.stdin.isatty():
-        with io.BytesIO() as iobytes:
-            tee(sys.stdin.buffer, iobytes, sys.stdout.buffer)
-            script = iobytes.getvalue().decode("utf-8")
-
-    # Disallow interactive without `-i` input (as potentially confusing)
-    else:
-        print("Careful, you forgot to tell me with which command you "
-              "need help!")
-        sys.exit(1)
+        temp.seek(0)
+        script = temp.read().decode().replace("\r\n", "\n")
 
     termcolor.cprint("\nAsking for help...\n", "yellow")
 
-    help = get_help(script)
-    render_help(help)
-
-    # data = {
-        # "cmd": " ".join(sys.argv[1:]),
-        # "format": "ans",
-        # "script": script,
-        # "username": os.environ.get("C9_USER")
-    # }
-
-    # # Read help50 url and from environment variable,
-    # # defaulting to help.cs50.net
-    # app_url = os.environ.get("APP_URL") or "https://help.cs50.net/"
-
-    # # Get number of columns in terminal, defaulting to 80
-    # columns, _ = shutil.get_terminal_size()
-    # if columns == 0: columns = 80
-
-    # # Connect to server and print response, showing error message if unable to
-    # try:
-        # # Connect to server
-        # r = requests.post(app_url, data)
-    # except requests.exceptions.RequestException:
-        # termcolor.cprint(wrap("Ack, there seems to be a bug in help50! "
-                              # "Please let sysadmins@cs50.harvard.edu know "
-                              # "with which error you need help!",
-                              # columns),
-                         # "yellow", end="")
-        # sys.exit(1)
-    # else:
-        # # Overwrite previous line with spaces
-        # print("\r{}\r".format(" " * columns), end="")
-        # termcolor.cprint(wrap(r.text.encode("utf-8").decode("unicode_escape"),
-                              # columns),
-                         # "yellow", end="")
-
-
-def tee(input, *outputs):
-    """Executes command, piping stdout and stderr to *outputs."""
-    while True:
-        try:
-            byte = input.read(1)
-        except EOFError:
-            byte = None
-
-        if not byte:
-            break
-
-        for output in outputs:
-            output.write(byte)
-            output.flush()
-
-
-def wrap(lines, columns):
-    """ Wraps a string to the specified number of columns,
-        preserving blank lines.
-    """
-    return "\n".join(
-        ("\n".join(textwrap.wrap(line, columns))
-         for line in lines))
+    render_help(get_help(script))
 
 
 if __name__ == "__main__":
